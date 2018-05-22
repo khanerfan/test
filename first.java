70c4402de5060c67ab828690b17af9d9a76961fb
tld <Erfan.Khan@synechron.com>
2018-05-22 11:47:08 +0530

    Second version of java file

--- first.java
+++ first.java
@@ -2,6 +2,11 @@ public class first
 {
  public static void main(String args[])
  {
+	 int i=10;
 	 System.out.println("Hello");
+	 if(i<15)
+		 System.out.println("Smaller Number")
+	 else
+		 System.out.println("Greater Number")
  }
 }
